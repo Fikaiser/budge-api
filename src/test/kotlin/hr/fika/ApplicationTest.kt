d8acdf809be57140592c9fe97040d0fb7607019c
Fikaiser <borna.fikajz@gmail.com>
2022-12-03 14:44:05 +0100

    Initial commit

---
+++ src/test/kotlin/hr/fika/ApplicationTest.kt
@@ -0,0 +1,42 @@
+package hr.fika
+
+import io.ktor.server.routing.*
+import io.ktor.http.*
+import io.ktor.serialization.gson.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import io.ktor.utils.io.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.io.InputStream
+import java.util.*
+import io.ktor.server.auth.*
+import io.ktor.util.*
+import io.ktor.server.auth.jwt.*
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
+import io.ktor.server.locations.*
+import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+import hr.fika.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
